8ffe7bcfed6a8acb70f302315276f0e2b5d8e453
jericokatong <jericokatong@gmail.com>
2024-08-19 23:16:46 +0800

    update: cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { Get, Post, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
+    origin: '*',
     methods: '*',
+    credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
