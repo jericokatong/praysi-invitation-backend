6eb52e4a31f7f3a2189aab927f910a3a4ee70e12
jericokatong <jericokatong@gmail.com>
2024-08-20 15:35:16 +0800

    update: cors

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Konfigurasi CORS
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://praysi-invitation.vercel.app'], // Ganti dengan origin frontend Anda
+    origin: 'https://praysi-invitation.vercel.app', // Ganti dengan origin frontend Anda
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Metode yang diizinkan
     allowedHeaders: ['Content-Type', 'Authorization'], // Header yang diizinkan
     credentials: true, // Jika menggunakan cookies atau authorization headers
-    preflightContinue: true,
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
